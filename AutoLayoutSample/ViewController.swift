5b3ad9faac32b1fbc47d2d50757f04e467224db5
haozyc <Antony@antonys-macbook-pro.local>
2015-10-03 01:09:52 +0800

    Initial Commit

---
+++ AutoLayoutSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AutoLayoutSample
+//
+//  Created by Antony on 3/10/2015.
+//  Copyright © 2015 OnUs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
